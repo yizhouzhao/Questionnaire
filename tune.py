2c14e4aa9895078a9c05f22394c6bb5c126b2e44
Yizhou <yizhouzhao@g.ucla.edu>
2021-01-05 20:06:10 -0800

    update xgboost

--- tune.py
+++ tune.py
@@ -12,7 +12,7 @@ epochs = 100
 learning_rate = 2e-5
 max_input_length = 100
 max_train_samples = 1024
-max_test_samples = None
+max_test_samples = 1024
 
 def calculate_accuracy(logits, labels):
     return (torch.sum(torch.argmax(logits,dim=1) == labels) / len(labels)).item()
@@ -66,12 +66,12 @@ def train(dataset_name:str, model_name:str):
             loss.backward()
             optimizer.step()
 
-            train_acc_list.append(loss.item())
+            #train_acc_list.append(loss.item())
 
             #if len(train_acc_list) % 20 == 0:
             #    print("training loss:", loss.item())
 
-            #train_acc_list.append(calculate_accuracy(output_logits, batch_label))
+            train_acc_list.append(calculate_accuracy(output_logits, batch_label))
 
         #------------------------------------------Test---------------------------------------------
         model.eval()
@@ -93,7 +93,7 @@ def train(dataset_name:str, model_name:str):
 
 
 if __name__ == '__main__':
-    dataset_name = "yelp_polarity" # yelp_polarity rotten_tomatoes
+    dataset_name = "rotten_tomatoes" # yelp_polarity rotten_tomatoes
     model_name = "XLNet" # "XLNet" # Roberta
 
     train(dataset_name, model_name)
